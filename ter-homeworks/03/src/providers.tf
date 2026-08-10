91355a7a098f0c8949d63a101f7f59e9dac5d1e7
hiriyanovmm <m.hiriyanov@yandex.ru>
2026-08-10 13:57:33 +0000

    change

--- ter-homeworks/03/src/providers.tf
+++ ter-homeworks/03/src/providers.tf
@@ -4,11 +4,11 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
-  required_version = "~>1.12.0"
+  required_version = ">1.12.0"
 }
 
 provider "yandex" {
-  token     = var.token
+  service_account_key_file = file("~/.ssh/yc_svc_authorized_key.json")
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = var.default_zone
